acd2cecf45743c9ec0d66fb086ea8c5a0c1142c0
kijapkin <kijapkin@gmail.com>
2018-11-04 15:49:34 +0200

    start

---
+++ vika/header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>OGBORDJOR</title>
+	<link rel="stylesheet" href="main.css">
+</head>
+<body>
+	<div id="wrapper">
+		<div class="header">
+			<a class="logo" href="index.html"></a>
+			<h1 class="head-tex">Site for Vika&Stiven Ogbordjor will be here soon!</h1>
+		</div>
