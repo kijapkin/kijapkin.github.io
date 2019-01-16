4427555e535de104fa27d5da22e9f3fa9087cb3a
kijapkin <kijapkin@gmail.com>
2019-01-16 19:04:50 +0200

    serfsez31

--- WIENS/dist/js/main.js
+++ WIENS/dist/js/main.js
@@ -1,3 +1,21 @@
 $(document).ready(function(){
-	
+
+	$('.multiple-items .wr-slide').css({
+		"display": "block",
+	});
+	$('.multiple-items').slick({
+	  infinite: true,
+	  slidesToShow: 1,
+	  slidesToScroll: 1,
+	  dots: false,
+	  arrows: false,
+	});
+	jQuery(".white-tringle .block-arrows .prev").click(function(){
+		console.log(true);
+    $('.multiple-items').slick('slickPrev');
+  	});
+ 	 jQuery(".white-tringle .block-arrows .next").click(function(){
+ 	 	console.log(true);
+    $('.multiple-items').slick('slickNext');
+  	});
 });
