4c03904d33203e3bf1792fee1d79d76d657b34e9
kijapkin <kijapkin@gmail.com>
2019-02-14 00:23:41 +0200

    srsfsezrg43235fw3r

--- landwill/dist/js/main.js
+++ landwill/dist/js/main.js
@@ -1,5 +1,6 @@
 $('.slider-main-top').slick({
   dots: false,
+  adaptiveHeight: true,
   prevArrow: $('.arrow-prew-main'),
   nextArrow: $('.arrow-next-main'),
 });
@@ -35,25 +36,58 @@ $('.slider-block-cards').slick({
 });
 
 $('.block-slider-system-profile').slick({
- dots: true,
-  infinite: true,
+  dots: false,
+  infinite: false,
   speed: 300,
   slidesToShow: 3,
+  variableWidth: true,
   slidesToScroll: 1,
-  prevArrow: $(''),
-  nextArrow: $(''),
+  prevArrow: $('.prew-profile'),
+  nextArrow: $('.next-profile'),
    responsive: [
     {
       breakpoint: 1200,
       settings: {
         slidesToShow: 2,
         slidesToScroll: 1,
-        infinite: true,
-        dots: true,
+        infinite: false,
+        dots: false,
       }
     },
     {
-      breakpoint: 700,
+      breakpoint: 992,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: false,
+        dots: true,
+        centerMode: true,
+      }
+    },
+    {
+      breakpoint: 770,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: false,
+        dots: true,
+      }
+    },
+  ]
+});
+
+$('.block-slider-system-divider').slick({
+  dots: false,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 1,
+  centerMode: true,
+  variableWidth: true,
+  prevArrow: $('.prew-divider'),
+  nextArrow: $('.next-divider'),
+  responsive: [
+    {
+      breakpoint: 1199,
       settings: {
         slidesToShow: 1,
         slidesToScroll: 1,
@@ -64,14 +98,6 @@ $('.block-slider-system-profile').slick({
   ]
 });
 
-$('.block-slider-system-divider').slick({
-  dots: true,
-  infinite: true,
-  speed: 300,
-  slidesToShow: 1,
-  centerMode: true,
-  variableWidth: true,
-});
 $('.slider-works').slick({
   dots: false,
   infinite: true,
@@ -91,24 +117,57 @@ $('.slider-works').slick({
       }
     },
     {
-      breakpoint: 600,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 2
-      }
-    },
-    {
-      breakpoint: 480,
+      breakpoint: 768,
       settings: {
         slidesToShow: 1,
-        slidesToScroll: 1
+        slidesToScroll: 1,
       }
-    }
+    },
   ]
 });
 
 $('.slider-feedback').slick({
-  dots: false,
+  dots: true,
   prevArrow: $('.prew-feedback'),
   nextArrow: $('.next-feedback'),
+});
+
+
+$('.block-cards-news').slick({
+  dots: false,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  prevArrow: $(''),
+  nextArrow: $(''),
+  responsive: [
+    {
+      breakpoint: 1300,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: false,
+        dots: false,
+      }
+    },
+    {
+      breakpoint: 720,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        infinite: false,
+        dots: true,
+      }
+    },
+    {
+      breakpoint: 500,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: false,
+        dots: true,
+      }
+    },
+  ]
 });
