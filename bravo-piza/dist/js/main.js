e7fd1436df6a68cab826fd6aad48934c5c4e023d
kijapkin <kijapkin@gmail.com>
2019-04-15 01:17:31 +0300

    sfefse3bwstwatwb3

--- bravo-piza/dist/js/main.js
+++ bravo-piza/dist/js/main.js
@@ -1,3 +1,13 @@
 $(document).ready(function(){
-	
+let popup 			= document.getElementById('formPopup'),
+	popupButton		= document.getElementById('popupButton');
+
+	popupButton.onclick = function(){
+		popup.style.display="block";
+	};
+	window.onclick = function (event) {
+		if(event.target == popup){
+			popup.style.display="none";
+		}
+	}
 });
