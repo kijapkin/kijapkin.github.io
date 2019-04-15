ad119a6125e01acfeba1403aaac25183dc08a10a
kijapkin <kijapkin@gmail.com>
2019-04-16 02:03:46 +0300

    6476tghfh

--- bravo-piza/dist/js/main.js
+++ bravo-piza/dist/js/main.js
@@ -1,9 +1,13 @@
 $(document).ready(function(){
-let popup 			= document.getElementById('formPopup'),
+let popup 			= document.getElementById('formPopup');
 	popupButton		= document.getElementById('popupButton');
+	toogleClose		= document.getElementById('toogleClose');
 
 	popupButton.onclick = function(){
 		popup.style.display="block";
+	};
+	toogleClose.onclick = function(){
+		popup.style.display="none";
 	};
 	window.onclick = function(e){
 		if(e.target==popup){
