81ba8d71563d2b845cfdfd2e432cb8622e67b7d1
kijapkin <kijapkin@gmail.com>
2019-05-16 12:06:25 +0300

    rsefw3fs

--- flamingo/dist/js/main.js
+++ flamingo/dist/js/main.js
@@ -1,3 +1,26 @@
-$(document).ready(function(){
-	
+$(document).ready(function() {
+    var $picAll = $('.halfSlide'),
+        picLength = $('.halfSlide').length,
+        picCur = 0,
+        topDefault = 35,
+        topCur = 0;
+    $picAll.not(":eq(0)").css({ 'top': '100%' });
+    $('body').on('mousewheel', function(e) {
+        if (e.originalEvent.wheelDelta / 120 > 0) {
+            if (picCur > 0) {
+                topCur = 100;
+                $picAll.eq(picCur).css({ 'top': topCur + '%' });
+                picCur--;
+            }
+        } else {
+            if (picCur < picLength - 1) {
+                picCur++;
+                topCur = topDefault;
+                if ($picAll.eq(picCur).hasClass('slide')) {
+                    topCur = 0;
+                }
+                $picAll.eq(picCur).css({ 'top': topCur + '%' });
+            }
+        }
+    });
 });
