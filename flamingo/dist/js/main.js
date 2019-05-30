32f25195b2e2f3fe64ef75f2f6dd0f8ab6b6cb58
kijapkin <kijapkin@gmail.com>
2019-05-30 18:51:20 +0300

    rfsse

--- flamingo/dist/js/main.js
+++ flamingo/dist/js/main.js
@@ -1,26 +1,62 @@
-$(document).ready(function() {
+$(document).ready(function(){
     var $picAll = $('.halfSlide'),
-        picLength = $('.halfSlide').length,
+        picLength = $picAll.length,
         picCur = 0,
-        topDefault = 35,
+        topDefault = 0,
         topCur = 0;
-    $picAll.not(":eq(0)").css({ 'top': '100%' });
-    $('body').on('mousewheel', function(e) {
-        if (e.originalEvent.wheelDelta / 120 > 0) {
+
+    $picAll.not(":eq(0)").css({'top': '100%'});
+
+    var lock = false;
+
+    $('#mainWrapper').on('mousewheel', function(e){
+        if(lock){
+            return false;
+        };
+
+        if(e.originalEvent.wheelDelta /120 > 0) {
             if (picCur > 0) {
-                topCur = 100;
-                $picAll.eq(picCur).css({ 'top': topCur + '%' });
+                lock = true;
+
+                //picCur--;
+
+                if (!$picAll.eq(picCur).hasClass('slide')){
+                    topCur = 150;
+                }else{
+                    topCur = 100;
+                };
+
+                $picAll.eq(picCur)
+                .animate({
+                    top: topCur+'%'
+                }, 200, function() {
+                    lock = false;
+                });
+
                 picCur--;
+
+                //$picAll.eq(picCur).css({'top': topCur+'%'});
             }
         } else {
-            if (picCur < picLength - 1) {
+            if (picCur < picLength-1) {
+                lock = true;
+
                 picCur++;
                 topCur = topDefault;
-                if ($picAll.eq(picCur).hasClass('slide')) {
-                    topCur = 0;
-                }
-                $picAll.eq(picCur).css({ 'top': topCur + '%' });
+
+                if (!$picAll.eq(picCur).hasClass('slide')) {
+                    topCur = 50;
+                };
+
+                $picAll.eq(picCur)
+                .animate({
+                    top: topCur+'%'
+                }, 200, function() {
+                    lock = false;
+                });
+                //$picAll.eq(picCur).css({'top': topCur+'%'});
             }
-        }
+        };
+        return false;
     });
 });
