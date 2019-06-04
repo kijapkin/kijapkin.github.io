38fbd78fe6e8c4a60734d6a1354a28f93adc0ee8
kijapkin <kijapkin@gmail.com>
2019-06-04 14:44:00 +0300

    srefw3twwDQ2R3WFA3D2QR3FD

---
+++ flamingo/public_html/js/main.js
@@ -0,0 +1,64 @@
+$(document).ready(function(){
+    var $picAll = $('.halfSlide'),
+        picLength = $picAll.length,
+        picCur = 0,
+        topDefault = 0,
+        topCur = 0;
+
+    $picAll.not(":eq(0)").css({'top': '100%'});
+
+    var lock = false;
+
+    $('#mainWrapper').on('mousewheel', function(e){
+        if(lock){
+            return false;
+        };
+
+        if(e.originalEvent.wheelDelta /120 > 0) {
+            if (picCur > 0) {
+                lock = true;
+
+                //picCur--;
+
+                if (!$picAll.eq(picCur).hasClass('slide')){
+                    topCur = 150;
+                }else{
+                    topCur = 100;
+                };
+
+                $picAll.eq(picCur)
+                .animate({
+                    top: topCur+'%'
+                }, 200, function() {
+                    lock = false;
+                });
+
+                picCur--;
+
+                //$picAll.eq(picCur).css({'top': topCur+'%'});
+            }
+        } else {
+            if (picCur < picLength-1) {
+                lock = true;
+
+                picCur++;
+                topCur = topDefault;
+
+                if (!$picAll.eq(picCur).hasClass('slide')) {
+                    topCur = 50;
+                };
+
+                $picAll.eq(picCur)
+                .animate({
+                    top: topCur+'%'
+                }, 200, function() {
+                    lock = false;
+                });
+
+                //$picAll.eq(picCur).css({'top': topCur+'%'});
+            }
+        };
+
+        return false;
+    });
+});
