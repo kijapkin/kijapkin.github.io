5f491612b12cf04febaccdfe0f2359d05b68261f
kijapkin <kijapkin@gmail.com>
2019-06-05 07:44:33 +0300

    eg4ew3ete4t

--- flamingo/dist/js/main.js
+++ flamingo/dist/js/main.js
@@ -1,66 +1,66 @@
-$(document).ready(function(){
-    var $picAll = $('.halfSlide'),
-        picLength = $picAll.length,
-        picCur = 0,
-        topDefault = 0,
-        topCur = 0;
+// $(document).ready(function(){
+//     var $picAll = $('.halfSlide'),
+//         picLength = $picAll.length,
+//         picCur = 0,
+//         topDefault = 0,
+//         topCur = 0;
 
-    $picAll.not(":eq(0)").css({'top': '100%'});
+//     $picAll.not(":eq(0)").css({'top': '100%'});
 
-    var lock = false;
+//     var lock = false;
 
-    $(document).on('mousewheel', '#mainWrapper', function(e){
-        if(lock){
-            return false;
-        };
+//     $(document).on('mousewheel', '#mainWrapper', function(e){
+//         if(lock){
+//             return false;
+//         };
 
-        //console.log('offsetY: '+e.originalEvent.offsetY);
-        console.log('pageY: '+e.originalEvent.pageY);
-        return true;
+//         //console.log('offsetY: '+e.originalEvent.offsetY);
+//         console.log('pageY: '+e.originalEvent.pageY);
+//         return true;
 
-        if(e.originalEvent.wheelDelta /120 > 0) {
-            if (picCur > 0) {
-                lock = true;
+//         if(e.originalEvent.wheelDelta /120 > 0) {
+//             if (picCur > 0) {
+//                 lock = true;
 
-                //picCur--;
+//                 //picCur--;
 
-                if (!$picAll.eq(picCur).hasClass('slide')){
-                    topCur = 150;
-                }else{
-                    topCur = 100;
-                };
+//                 if (!$picAll.eq(picCur).hasClass('slide')){
+//                     topCur = 150;
+//                 }else{
+//                     topCur = 100;
+//                 };
 
-                $picAll.eq(picCur)
-                .animate({
-                    top: topCur+'%'
-                }, 200, function() {
-                    lock = false;
-                });
+//                 $picAll.eq(picCur)
+//                 .animate({
+//                     top: topCur+'%'
+//                 }, 200, function() {
+//                     lock = false;
+//                 });
 
-                picCur--;
+//                 picCur--;
 
-                //$picAll.eq(picCur).css({'top': topCur+'%'});
-            }
-        } else {
-            if (picCur < picLength-1) {
-                lock = true;
+//                 //$picAll.eq(picCur).css({'top': topCur+'%'});
+//             }
+//         } else {
+//             if (picCur < picLength-1) {
+//                 lock = true;
 
-                picCur++;
-                topCur = topDefault;
+//                 picCur++;
+//                 topCur = topDefault;
 
-                if (!$picAll.eq(picCur).hasClass('slide')) {
-                    topCur = 50;
-                };
+//                 if (!$picAll.eq(picCur).hasClass('slide')) {
+//                     topCur = 50;
+//                 };
 
-                $picAll.eq(picCur)
-                .animate({
-                    top: topCur+'%'
-                }, 200, function() {
-                    lock = false;
-                });
-                //$picAll.eq(picCur).css({'top': topCur+'%'});
-            }
-        };
-        return false;
-    });
-});
+//                 $picAll.eq(picCur)
+//                 .animate({
+//                     top: topCur+'%'
+//                 }, 200, function() {
+//                     lock = false;
+//                 });
+//                 //$picAll.eq(picCur).css({'top': topCur+'%'});
+//             }
+//         };
+//         return false;
+//     });
+// });
