8762d68fa850d431dd9a8ca3bdeca32c24d01477
kijapkin <kijapkin@gmail.com>
2019-06-06 05:46:10 +0300

    rfw3t3fr

--- flamingo/dist/js/main.js
+++ flamingo/dist/js/main.js
@@ -64,3 +64,12 @@
 //         return false;
 //     });
 // });
+
+new fullpage('#fullpage', {
+    //options here
+    autoScrolling: true,
+    scrollHorizontally: true
+});
+
+//methods
+// fullpage_api.setAllowScrolling(false);
